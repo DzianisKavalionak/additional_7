a82baeb5d0c27337ca9294e4f9007dd693d3c96b
DzianisKavalionak <nereidy4@gmail.com>
2018-10-03 18:59:16 +0300

    100% ref0.1

--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
 module.exports = function solveSudoku(matrix) {
     let str = matrix;
-
-
-
-
     rewriteZeros();
-
     function rewriteZeros() {
         for (let x = 0; x < str.length; x++) {
             for (let y = 0; y < str.length; y++) {
                 if (str[x][y] === 0) {
                     str[x][y] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
                     searchNumbers(x, y);
-                }
-            }
-        }
-    }
-    var abc = 0;
-    function superSudoku() {
-        if (abc < 10000) {
-            abc++;
-            for (let x = 0; x < str.length; x++) {
-                for (let y = 0; y < str.length; y++) {
-                    if (typeof(str[x][y]) === "object") {
-                        searchNumbers(x, y);
-                    }
                 }
             }
         }
@@ -49,121 +31,23 @@ module.exports = function solveSudoku(matrix) {
                 }
             }
         }
-        if (x < 3 && y < 3) {
-            for (let i = 0; i < 3; i++) {
-                for (let j = 0; j < 3; j++) {
-                    for (let k = 0; k < str.length; k++) {
-                        if (typeof(str[i][j]) === "number" && str[i][j] === str[x][y][k]) {
-                            str[x][y].splice(k, 1);
-                        }
-                    }
-                }
-            }
-        }
-        else
-        if (x < 6 && y < 3 && x > 3) {
-            for (let i = 3; i < 6; i++) {
-                for (let j = 0; j < 3; j++) {
-                    for (let k = 0; k < str.length; k++) {
-                        if (typeof(str[i][j]) === "number" && str[i][j] === str[x][y][k]) {
-                            str[x][y].splice(k, 1);
-                        }
-                    }
-                }
-            }
-        }
-        else
-        if (x < 9 && y < 3 && x > 6) {
-            for (let i = 6; i < 9; i++) {
-                for (let j = 0; j < 3; j++) {
-                    for (let k = 0; k < str.length; k++) {
-                        if (typeof(str[i][j]) === "number" && str[i][j] === str[x][y][k]) {
-                            str[x][y].splice(k, 1);
-                        }
-                    }
-                }
-            }
-        }
-        else
-        if (x < 3 && y < 6 && y > 3) {
-            for (let i = 0; i < 3; i++) {
-                for (let j = 3; j < 6; j++) {
-                    for (let k = 0; k < str.length; k++) {
-                        if (typeof(str[i][j]) === "number" && str[i][j] === str[x][y][k]) {
-                            str[x][y].splice(k, 1);
-                        }
-                    }
-                }
-            }
-        }
-        else
-        if (x < 3 && y < 9 && y > 6) {
-            for (let i = 0; i < 3; i++) {
-                for (let j = 6; j < 9; j++) {
-                    for (let k = 0; k < str.length; k++) {
-                        if (typeof(str[i][j]) === "number" && str[i][j] === str[x][y][k]) {
-                            str[x][y].splice(k, 1);
-                        }
-                    }
-                }
-            }
-        }
-        else
-        if (x < 6 && y < 6 && y > 3 && x > 3) {
-            for (let i = 3; i < 6; i++) {
-                for (let j = 3; j < 6; j++) {
-                    for (let k = 0; k < str.length; k++) {
-                        if (typeof(str[i][j]) === "number" && str[i][j] === str[x][y][k]) {
-                            str[x][y].splice(k, 1);
-                        }
-                    }
-                }
-            }
-        }
-        else
-        if (x < 9 && y < 6 && y > 3 && x > 6) {
-            for (let i = 6; i < 9; i++) {
-                for (let j = 3; j < 6; j++) {
-                    for (let k = 0; k < str.length; k++) {
-                        if (typeof(str[i][j]) === "number" && str[i][j] === str[x][y][k]) {
-                            str[x][y].splice(k, 1);
-                        }
-                    }
-                }
-            }
-        }
-        else
-        if (x < 6 && y < 9 && y > 6 && x > 3) {
-            for (let i = 3; i < 6; i++) {
-                for (let j = 6; j < 9; j++) {
-                    for (let k = 0; k < str.length; k++) {
-                        if (typeof(str[i][j]) === "number" && str[i][j] === str[x][y][k]) {
-                            str[x][y].splice(k, 1);
-                        }
-                    }
-                }
-            }
-        }
-        else
-        if (x < 9 && y < 9 && y > 6 && x > 6) {
-            for (let i = 6; i < 9; i++) {
-                for (let j = 6; j < 9; j++) {
-                    for (let k = 0; k < str.length; k++) {
-                        if (typeof(str[i][j]) === "number" && str[i][j] === str[x][y][k]) {
-                            str[x][y].splice(k, 1);
-                        }
-                    }
-                }
-            }
-        }
 
+        let a , b = 0;
+        a = (x < 3)? a = 0 : (x < 6)? a = 3 : a = 6;
+        b = (y < 3)? b = 0 : (y < 6)? b = 3 : b = 6;
 
-
-
+        for (let i = a, lena = a + 3; i < lena; i++) {
+            for (let k = b, lenb = b + 3; k < lenb; k++) {
+                for (let p = 0, lenp = str[x][y].length; p < str; p++) {
+                    if (typeof(str[i][k]) === "number" && str[i][k] === str[x][y][p]) {
+                        str[x][y].splice(p, 1);
+                    }
+                }
+            }
+        }
         if(str[x][y].length === 1) {
             str[x][y] = str[x][y][0];
         }
-        superSudoku();
     }
     return str;
-}
+};
